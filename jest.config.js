1435368d4da47c0db4f08f07e7026a65a76fe752
Dylan <dw8819@yahoo.com>
2020-12-28 13:25:29 -0500

    Checkpoint for base setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  "roots": [
+    "<rootDir>/src"
+  ]
+};
---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { testEnvironment } from "../jest.config"
+
+describe('Blank Test', () => {
+  test('add', async () => {
+    expect(1 + 1).toEqual(2);
+  })
+});
